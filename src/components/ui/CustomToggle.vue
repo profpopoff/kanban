0763fea1661d76b9672e6a6328279ad87219259c
Aleksei Ivanov <profpopoff@gmail.com>
2023-03-01 14:24:39 +0300

    Create custom toggle ui component

---
+++ src/components/ui/CustomToggle.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+const props = defineProps({
+   isChecked: {
+      type: Boolean,
+      default: false,
+   }
+})
+</script>
+
+<template>
+   <div class="custom-toggle">
+      <input type="checkbox" name="toggle" id="toggle" :checked="isChecked">
+      <label for="toggle">
+         <slot />
+      </label>
+   </div>
+</template>
+
+<style scoped lang="scss">
+.custom-toggle {
+   display: flex;
+   align-items: center;
+   gap: .5em;
+
+   label {
+      cursor: pointer;
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+   }
+
+   input {
+      --height: 1.5em;
+      --padding: .3em;
+
+      position: relative;
+      width: 3em;
+      height: var(--height);
+      background: #696969;
+      /* background: hsl(var(--grey-color)); */
+      border-radius: 2em;
+      cursor: pointer;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      transition: all 0.3s ease-in-out;
+
+      &::after {
+         content: "";
+         display: inline-block;
+         position: absolute;
+         left: var(--padding);
+         top: 50%;
+         width: calc(var(--height) - var(--padding) * 2);
+         aspect-ratio: 1;
+         background-color: #fff;
+         border-radius: 50%;
+         transform: translate(0, -50%);
+         transition: all 0.3s cubic-bezier(0.2, 0.85, 0.32, 1.4);
+      }
+
+      &:checked {
+         /* background-color: hsl(var(--accent-color)); */
+         background-color: #640101;
+
+         &::after {
+            left: calc(100% - (var(--height) - var(--padding)));
+         }
+      }
+   }
+}
+</style>
