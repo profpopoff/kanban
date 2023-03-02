1c05b7afcc9689332f3aa0c0276543b3317ac064
Aleksei Ivanov <profpopoff@gmail.com>
2023-03-02 17:24:50 +0300

    Add app logo

--- src/components/layout/Sidebar/Sidebar.vue
+++ src/components/layout/Sidebar/Sidebar.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import AppLogo from './Logo.vue'
 import DarkModeToggle from './DarkModeToggle.vue'
 
 </script>
 
 <template>
    <div class="sidebar">
-      <h2>sidebar</h2>
+      <header>
+         <AppLogo />
+      </header>
       <DarkModeToggle />
    </div>
 </template>
@@ -13,7 +16,17 @@ import DarkModeToggle from './DarkModeToggle.vue'
 <style scoped lang="scss">
 .sidebar {
    grid-area: sidebar;
+
    background-color: hsl(var(--element-color));
    border-right: 1px solid hsl(var(--grey-color) / .25);
+   transition: var(--dark-theme-transition);
+
+   padding-inline: 1.5rem;
+
+   header {
+      height: 6rem;
+      display: flex;
+      align-items: center;
+   }
 }
 </style>
