de6c7fd13c5b6c49b8b923a18d3fce7837110592
Aleksei Ivanov <profpopoff@gmail.com>
2023-03-04 13:16:41 +0300

    Add custom button component

---
+++ src/components/ui/CustomButton.vue
@@ -0,0 +1,23 @@
+<template>
+   <button class="custom-button">
+      <slot />
+   </button>
+</template>
+
+<style scoped lang="scss">
+.custom-button {
+   padding: 1rem 1.5rem;
+   border-radius: 2rem;
+   border: none;
+   background-color: hsl(var(--accent-color));
+   color: #fff;
+   font-weight: 600;
+   text-transform: capitalize;
+   line-height: 1;
+   cursor: pointer;
+
+   &:empty {
+      display: none;
+   }
+}
+</style>
