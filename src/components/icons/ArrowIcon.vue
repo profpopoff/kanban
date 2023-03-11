de5b4fd8ec6d32320e1756db8a637816064def1c
Aleksei Ivanov <profpopoff@gmail.com>
2023-03-11 21:01:08 +0300

    Add arrow icon

---
+++ src/components/icons/ArrowIcon.vue
@@ -0,0 +1,13 @@
+<template>
+   <svg version="1.1" id="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 317.7" xml:space="preserve">
+      <path class="st0"
+         d="M502.8,233.7L278.2,9.1C272.1,3,264,0,256,0c-8,0-16.1,3-22.2,9.1L9.2,233.7c-12.2,12.2-12.2,32,0,44.2
+      	l30.7,30.7c12.2,12.2,32,12.2,44.2,0l172-172l172,172c12.2,12.2,32,12.2,44.2,0l30.7-30.7C515,265.7,515,245.9,502.8,233.7z" />
+   </svg>
+</template>
+
+<style scoped>
+svg {
+   fill: hsl(var(--accent-color));
+}
+</style>
